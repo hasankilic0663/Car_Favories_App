ce57e4d9988642cba604996b70f625ad5abd0c4f
Knife Club <hasankilic@hotmail.com>
2024-05-04 18:39:48 +0300

    menu item created

--- app/src/main/java/com/hasankilic/car_favories_app/ArtActivity.kt
+++ app/src/main/java/com/hasankilic/car_favories_app/ArtActivity.kt
@@ -2,10 +2,30 @@ package com.hasankilic.car_favories_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.hasankilic.car_favories_app.databinding.ActivityArtBinding
 
 class ArtActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityArtBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_art)
+        binding=ActivityArtBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+    }
+
+
+
+
+    fun saveButtonClicked(view: View){
+
+
+
+    }
+
+    fun selectImage(view: View){
+
     }
 }
--- app/src/main/java/com/hasankilic/car_favories_app/MainActivity.kt
+++ app/src/main/java/com/hasankilic/car_favories_app/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.hasankilic.car_favories_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.hasankilic.car_favories_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {//baglama işlemi yapılcak
+
+        //inflater
+        val menuInflater= menuInflater
+        menuInflater.inflate(R.menu.art_menu,menu)
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {//menuye tıklanırsa ne yapacak
+
+        if (item.itemId==R.id.add_art_item){
+
+            val intent=Intent(this@MainActivity,ArtActivity::class.java)
+            startActivity(intent)
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
